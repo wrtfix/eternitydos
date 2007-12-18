8d52c494a48e4e5fccc6760385105f0ce24a0807
manuel.alonso.d@gmail.com <manuel.alonso.d@gmail.com@cf74a119-8741-0410-8f16-5df6d1adb2f7>
2007-12-18 21:52:42 +0000

    Le agregué una gilada para ver si anda el SVN, exitos

--- EternityFrm.cpp
+++ EternityFrm.cpp
@@ -54,63 +54,63 @@ void EternityFrm::CreateGUIControls()
 
 	SetTitle(wxT("Eternity"));
 	SetIcon(wxNullIcon);
-	SetSize(6,-12,517,496);
+	SetSize(8,8,517,496);
 	Center();
 	
 
-	Resolver = new wxButton(this, ID_RESOLVER, wxT("&Resolver"), wxPoint(34,315), wxSize(130,137), 0, wxDefaultValidator, wxT("Resolver"));
+	Resolver = new wxButton(this, ID_RESOLVER, wxT("&Resolver mucho"), wxPoint(34,315), wxSize(130,137), 0, wxDefaultValidator, wxT("Resolver"));
 	Resolver->SetForegroundColour(wxColour(255,255,255));
 	Resolver->SetBackgroundColour(wxColour(255,0,0));
 	Resolver->SetFont(wxFont(16, wxSWISS, wxNORMAL,wxBOLD, false, wxT("Tahoma")));
 
 	wxArrayString arrayStringFor_WxListBox1;
 	WxListBox1 = new wxListBox(this, ID_WXLISTBOX1, wxPoint(170,313), wxSize(303,140), arrayStringFor_WxListBox1, wxLB_SINGLE);
-	WxListBox1->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Microsoft Sans Serif")));
+	WxListBox1->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Tahoma")));
 
 	WxStaticBox5 = new wxStaticBox(this, ID_WXSTATICBOX5, wxT("Resolver"), wxPoint(23,297), wxSize(466,163));
-	WxStaticBox5->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Microsoft Sans Serif")));
+	WxStaticBox5->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Tahoma")));
 
 	Mezclar = new wxButton(this, ID_MEZCLAR, wxT("&Mezclar"), wxPoint(351,229), wxSize(75,25), 0, wxDefaultValidator, wxT("Mezclar"));
-	Mezclar->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Microsoft Sans Serif")));
+	Mezclar->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Tahoma")));
 
 	WxStaticBox4 = new wxStaticBox(this, ID_WXSTATICBOX4, wxT("Mezclar"), wxPoint(22,191), wxSize(467,97));
-	WxStaticBox4->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Microsoft Sans Serif")));
+	WxStaticBox4->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Tahoma")));
 
 	Crear = new wxButton(this, ID_CREAR, wxT("Crear"), wxPoint(351,146), wxSize(75,25), 0, wxDefaultValidator, wxT("Crear"));
-	Crear->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Microsoft Sans Serif")));
+	Crear->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Tahoma")));
 
 	WxStaticBox3 = new wxStaticBox(this, ID_WXSTATICBOX3, wxT("Crear"), wxPoint(268,13), wxSize(223,169));
-	WxStaticBox3->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Microsoft Sans Serif")));
+	WxStaticBox3->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Tahoma")));
 
 	WxSpinCtrl4 = new wxSpinCtrl(this, ID_WXSPINCTRL4, wxT("50"), wxPoint(187,136), wxSize(38,22), wxSP_ARROW_KEYS, 0, 100, 50);
-	WxSpinCtrl4->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Microsoft Sans Serif")));
+	WxSpinCtrl4->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Tahoma")));
 
 	WxStaticText4 = new wxStaticText(this, ID_WXSTATICTEXT4, wxT("Largo:"), wxPoint(150,142), wxDefaultSize, 0, wxT("WxStaticText4"));
-	WxStaticText4->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Microsoft Sans Serif")));
+	WxStaticText4->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Tahoma")));
 
 	WxStaticText3 = new wxStaticText(this, ID_WXSTATICTEXT3, wxT("Ancho:"), wxPoint(50,138), wxDefaultSize, 0, wxT("WxStaticText3"));
-	WxStaticText3->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Microsoft Sans Serif")));
+	WxStaticText3->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Tahoma")));
 
 	WxSpinCtrl3 = new wxSpinCtrl(this, ID_WXSPINCTRL3, wxT("50"), wxPoint(94,135), wxSize(36,22), wxSP_ARROW_KEYS, 0, 100, 50);
-	WxSpinCtrl3->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Microsoft Sans Serif")));
+	WxSpinCtrl3->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Tahoma")));
 
 	WxStaticBox2 = new wxStaticBox(this, ID_WXSTATICBOX2, wxT("Ficha:"), wxPoint(34,106), wxSize(202,62));
-	WxStaticBox2->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Microsoft Sans Serif")));
+	WxStaticBox2->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Tahoma")));
 
 	WxStaticText2 = new wxStaticText(this, ID_WXSTATICTEXT2, wxT("Colores:"), wxPoint(35,78), wxDefaultSize, 0, wxT("WxStaticText2"));
-	WxStaticText2->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Microsoft Sans Serif")));
+	WxStaticText2->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Tahoma")));
 
 	WxSpinCtrl2 = new wxSpinCtrl(this, ID_WXSPINCTRL2, wxT("2"), wxPoint(115,74), wxSize(121,22), wxSP_ARROW_KEYS, 2, 22, 2);
-	WxSpinCtrl2->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Microsoft Sans Serif")));
+	WxSpinCtrl2->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Tahoma")));
 
 	WxStaticText1 = new wxStaticText(this, ID_WXSTATICTEXT1, wxT("Tamaño:"), wxPoint(35,44), wxDefaultSize, 0, wxT("WxStaticText1"));
-	WxStaticText1->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Microsoft Sans Serif")));
+	WxStaticText1->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Tahoma")));
 
 	WxSpinCtrl1 = new wxSpinCtrl(this, ID_WXSPINCTRL1, wxT("2"), wxPoint(116,40), wxSize(121,22), wxSP_ARROW_KEYS, 2, 16, 2);
-	WxSpinCtrl1->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Microsoft Sans Serif")));
+	WxSpinCtrl1->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Tahoma")));
 
 	WxStaticBox1 = new wxStaticBox(this, ID_WXSTATICBOX1, wxT("Configuracion de tablero"), wxPoint(25,14), wxSize(223,167));
-	WxStaticBox1->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Microsoft Sans Serif")));
+	WxStaticBox1->SetFont(wxFont(8, wxSWISS, wxNORMAL,wxNORMAL, false, wxT("Tahoma")));
 	////GUI Items Creation End
 }
 
