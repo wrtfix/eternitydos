4c01171ae12c376c089e54e37e7cd266d53289bc
manuel.alonso.d@gmail.com <manuel.alonso.d@gmail.com@cf74a119-8741-0410-8f16-5df6d1adb2f7>
2007-12-19 21:50:45 +0000

    La nueva version con giro en la ficha

--- release/moc_clave.cpp
+++ release/moc_clave.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'clave.h'
 **
-** Created: Wed 19. Dec 09:54:28 2007
+** Created: Wed 19. Dec 17:21:31 2007
 **      by: The Qt Meta Object Compiler version 59 (Qt 4.3.3)
 **
 ** WARNING! All changes made in this file will be lost!
@@ -22,7 +22,7 @@ static const uint qt_meta_data_Clave[] = {
        1,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   10, // methods
+       6,   10, // methods
        0,    0, // properties
        0,    0, // enums/sets
 
@@ -30,6 +30,9 @@ static const uint qt_meta_data_Clave[] = {
        7,    6,    6,    6, 0x08,
       31,    6,    6,    6, 0x08,
       57,    6,    6,    6, 0x08,
+      91,   89,   84,    6, 0x08,
+     121,    6,  117,    6, 0x08,
+     135,    6,    6,    6, 0x08,
 
        0        // eod
 };
@@ -37,7 +40,9 @@ static const uint qt_meta_data_Clave[] = {
 static const char qt_meta_stringdata_Clave[] = {
     "Clave\0\0on_botonCrear_clicked()\0"
     "on_botonMezclar_clicked()\0"
-    "on_botonResolver_clicked()\0"
+    "on_botonResolver_clicked()\0bool\0,\0"
+    "perteneceTablero(int,int)\0int\0"
+    "posicion(int)\0on_botonGirar_clicked()\0"
 };
 
 const QMetaObject Clave::staticMetaObject = {
@@ -68,8 +73,13 @@ int Clave::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
         case 0: on_botonCrear_clicked(); break;
         case 1: on_botonMezclar_clicked(); break;
         case 2: on_botonResolver_clicked(); break;
+        case 3: { bool _r = perteneceTablero((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])));
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = _r; }  break;
+        case 4: { int _r = posicion((*reinterpret_cast< int(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = _r; }  break;
+        case 5: on_botonGirar_clicked(); break;
         }
-        _id -= 3;
+        _id -= 6;
     }
     return _id;
 }
